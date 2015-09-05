32d1f1ea1dd309bbd108f21750acd88d9a0e28f8
D3luxe <d3luxex@gmail.com>
2015-09-05 11:55:49 -0400

    Added game commands and fixed Give Gold

--- game/scripts/vscripts/util.lua
+++ game/scripts/vscripts/util.lua
@@ -55,7 +55,23 @@ COLOR_ORANGE = '\x1B'
 COLOR_LRED = '\x1C'
 COLOR_GOLD = '\x1D'
 
+function StringStartsWith( fullstring, substring )
+    local strlen = string.len(substring)
+    local first_characters = string.sub(fullstring, 1 , strlen)
+    return (first_characters == substring)
+end
 
+function split(inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            t[i] = str
+            i = i + 1
+    end
+    return t
+end
 
 --============ Copyright (c) Valve Corporation, All rights reserved. ==========
 --
